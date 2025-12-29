4ada5ba815baf9bb64f2857773829324422ec2c1
Fotis Kyriakidis <fot.kyriakidis@gmail.com>
2025-12-29 23:15:09 +0100

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -16,3 +16,24 @@ FROM accounts
 WHERE (name LIKE 'C%' OR name LIKE 'W%') 
               AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
               AND primary_poc NOT LIKE '%eana%');
+
+--Provide a table for all web_events associated with account name of Walmart. There should be three columns. Be sure to include the primary_poc, time of the event, and the channel for each event.
+--Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
+
+select w.occurred_at, w.channel, a.primary_poc, a.name
+from web_events as w
+join accounts as a
+on w.account_id = a.id
+where a.name = 'Walmart';
+
+--Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order
+--A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+
+select r.name as regions, a.name as accounts, o.total_amt_usd/(o.total+0.01) as unit_price 
+from sales_reps as s
+join region as r
+on s.region_id = r.id
+join accounts as a
+on s.id = a.sales_rep_id
+join orders as o
+on o.account_id = a.id;
